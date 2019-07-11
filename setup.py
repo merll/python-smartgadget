ce56e7c214abf26d2a362b4155443c8575441d68
Matthias Erll <matthias@erll.de>
2019-07-11 21:35:08 +0200

    Added readme import to setup.

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
-# -*- coding: utf-8 -*-
+from distutils.spawn import find_executable
+import os
+
 from setuptools import setup
+
+
+def include_readme():
+    try:
+        import pandoc
+    except ImportError:
+        return ''
+    pandoc.core.PANDOC_PATH = find_executable('pandoc')
+    readme_file = os.path.join(os.path.dirname(__file__), 'README.md')
+    doc = pandoc.Document()
+    with open(readme_file, 'r') as rf:
+        doc.markdown = rf.read()
+    return doc.rst.decode('utf-8')
+
 
 setup(
     name='python-smartgadget',
@@ -9,6 +25,7 @@ setup(
     install_requires=['bluepy>=1.0.5'],
     description='Library for reading temperature, humidity, and battery level from a '
                 'SHT31 Smart Gadget Development Kit by Sensirion',
+    long_description=include_readme(),
     author='Matthias Erll',
     author_email='matthias@erll.de',
     url='https://github.com/merll/python-smartgadget',
